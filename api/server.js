e563ad39ffb12c8daf7865203ea8c7f18fce04ad
serkantoraman <serkan@github.com>
2023-06-24 22:43:29 +0300

    project diagrame and knex migrate done, server is up and running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,20 @@
 // serverı buraya yazın ve index.js require yazın
+
+//Importlar
+const express=require('express');
+const server = express();
+
+
+//Middlewares
+server.use(express.json());
+
+
+//Routes
+
+
+//Errors
+
+
+//Exports
+
+module.exports = server;
